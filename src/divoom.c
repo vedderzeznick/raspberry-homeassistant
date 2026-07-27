fe7638ab74db11451a5a788a35c4d95976613839
Martin Alarcon <cristianma.2109@gmail.com>
2026-07-27 19:42:55 +0200

    divoom init

--- src/divoom.c
+++ src/divoom.c
@@ -18,7 +18,26 @@ static divoom_t divoom;
 
 int divoom_init(const char *device_path)
 {
+    DBusError err;
+
     memset(&divoom, 0, sizeof(divoom));
+
+    dbus_error_init(&err);
+
+    divoom.conn = dbus_bus_get(DBUS_BUS_SYSTEM, &err);
+
+    if (dbus_error_is_set(&err))
+    {
+        printf("DBus error: %s\n", err.message);
+        dbus_error_free(&err);
+        return -1;
+    }
+
+    if (divoom.conn == NULL)
+    {
+        printf("Cannot connect to system bus\n");
+        return -1;
+    }
 
     strncpy(divoom.device,
             device_path,
@@ -27,8 +46,9 @@ int divoom_init(const char *device_path)
     strcpy(divoom.write_char,
            "/org/bluez/hci0/dev_B1_21_81_3F_84_7C/service0006/char000c");
 
-    printf("Divoom device : %s\n", divoom.device);
-    printf("Write char    : %s\n", divoom.write_char);
+    printf("Connected to DBus\n");
+    printf("Device : %s\n", divoom.device);
+    printf("Write  : %s\n", divoom.write_char);
 
     return 0;
 }
